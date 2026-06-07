c7d46e0e6537ee6bc4b2df599de2d7340ab8c970
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-07 18:46:27 -0300

    Expose Compose dependencies in library metadata

--- emojislider/build.gradle.kts
+++ emojislider/build.gradle.kts
@@ -41,11 +41,11 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
+            api(compose.runtime)
+            api(compose.foundation)
+            api(compose.material3)
+            api(compose.ui)
+            api(compose.components.resources)
         }
 
         commonTest.dependencies {
